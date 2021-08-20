644b8b0c10c3c70a0e580560101c7260a7455d2c
leonardoleon1404 <A01740974@itesm.mx>
2021-08-20 23:57:36 +0000

    Ejercicios de tarea - Decisiones_Leonardo

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    indice = peso / altura**2
+
+    if peso<=0 or altura<=0:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
+    elif indice<20:
+        print("PESO BAJO")
+    elif 20<=indice and indice<25:
+        print("NORMAL")
+    elif 25<=indice and indice<30:
+        print("SOBREPESO")
+    elif 30<=indice and indice<40:
+        print("OBESIDAD")
+    elif 40<=indice:
+        print("OBESIDAD MORBIDA")
+    else:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
 
 if __name__=='__main__':
     main()
